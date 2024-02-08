26cb6f32a824b61842ce69994eba9b3eebda1ce2
sepj <serzh.matusevich@mail.ru>
2024-02-08 21:50:08 +0300

    Finalized beta
    approved stable build, changed interface, working upgrades and counters.

--- clicker 2/Assets/game.cs
+++ clicker 2/Assets/game.cs
@@ -6,87 +6,89 @@ using TMPro;
 
 public class Game : MonoBehaviour
 {
-
-    //click
-
     public TMP_Text scoreText;
-    public float currentScore;
+    public float cScore;
     public float hitPower;
-    public float scoreIncreasedPerSecond;
+    public float CPS;
     public float x;
 
-    //shop
-
-    public int shop1prize;
+    public int Building1Cost;
     public TMP_Text shop1text;
 
     public int shop2prize;
     public TMP_Text shop2text;
 
-    //display
-
     public TMP_Text amount1Text;
     public int amount1;
-    public float amount1Profit;
+    public float CPS1;
 
     public TMP_Text amount2Text;
     public int amount2;
-    public float amount2Profit;
-
-    //clickUpgrade
+    public float CPS2;
 
     public int upgradePrize;
     public TMP_Text upgradeText;
 
     void Start()
     {
-        currentScore = 0;
+        cScore = 0;
         hitPower = 1;
-        scoreIncreasedPerSecond = 1;
+        CPS = 1;
         x = 0f;
+
+        //
+        Building1Cost = 25;
+        shop2prize = 125;
+        amount1 = 0;
+        CPS1 = 1;
+        amount2 = 0;
+        CPS2 = 5;
+
+        //
+        //
+
+        // load
 
     }
 
     void Update()
     {
-        scoreText.text = (int)currentScore+" $";
-        scoreIncreasedPerSecond = x * Time.deltaTime;
-        currentScore = currentScore + scoreIncreasedPerSecond;
+        scoreText.text = (int)cScore + " ";
+        CPS = x * Time.deltaTime;
+        cScore = cScore + CPS;
 
-        shop1text.text = "Tier 1: " + shop1prize + " $";
-        shop2text.text = "Tier 2: " + shop2prize + " $";
+        shop1text.text = "Building 1: " + Building1Cost;
+        shop2text.text = "Building 2: " + shop2prize;
 
-        amount1Text.text = "Tier1: " + amount1 + "arts $:" + amount1Profit + "/s";
-        amount2Text.text = "Tier2: " + amount2 + "arts $:" + amount2Profit + "/s";
+        amount1Text.text = "Lvl 1: " + amount1 + "x, points/s:  " + CPS1;
+        amount2Text.text = "Lvl 2: " + amount2 + "x, points/s:  " + CPS2;
 
-        upgradeText.text = "cost: " + upgradePrize + " $";
-
+        upgradeText.text = "Upgrade click " + upgradePrize;
     }
-
-    public void Hit()
+        public void Hit()
     {
-        currentScore += hitPower;
+        cScore += hitPower;
     }
 
     public void Shop1()
     {
-        if (currentScore>=shop1prize)
+        if (cScore>=Building1Cost)
         {
-            currentScore -= shop1prize;
+            cScore -= Building1Cost;
             amount1 += 1;
-            amount1Profit += 1;
+            CPS1 += 1;
             x += 1;
-            shop1prize += 25;
+            Building1Cost += 25;
         }
     }
 
     public void Shop2()
     {
-        if (currentScore >= shop2prize)
+        if (cScore >= shop2prize)
         {
-            currentScore -= shop2prize;
+            cScore -= shop2prize;
             amount2 += 1;
-            amount2Profit += 5;
+            CPS2 += 5;
             x += 5;
             shop2prize += 125;
         }
@@ -94,9 +96,9 @@ public class Game : MonoBehaviour
 
     public void Upgrade()
     {
-        if (currentScore>=upgradePrize)
+        if (cScore>=upgradePrize)
         {
-            currentScore -= upgradePrize;
+            cScore -= upgradePrize;
             hitPower *= 2;
             upgradePrize *= 3;
         }
