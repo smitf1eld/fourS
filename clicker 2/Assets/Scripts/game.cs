5ca8bd8fb566c19094cefe1704922537ef742a9d
smitf1eld <MarkovskiyAK22@st.ithub.ru>
2024-02-09 05:31:14 +0300

    finish commit to check
    ssssss

--- clicker 2/Assets/Scripts/game.cs
+++ clicker 2/Assets/Scripts/game.cs
@@ -12,11 +12,14 @@ public class Game : MonoBehaviour
     public float CPS;
     public float x;
 
-    public int Building1Cost;
+    public int shop1prize;
     public TMP_Text shop1text;
 
     public int shop2prize;
     public TMP_Text shop2text;
+
+    public int shop3prize;
+    public TMP_Text shop3text;
 
     public TMP_Text amount1Text;
     public int amount1;
@@ -26,8 +29,14 @@ public class Game : MonoBehaviour
     public int amount2;
     public float CPS2;
 
+    public TMP_Text amount3Text;
+    public int amount3;
+    public float CPS3;
+
     public int upgradePrize;
     public TMP_Text upgradeText;
+
+
 
     void Start()
     {
@@ -35,14 +44,18 @@ public class Game : MonoBehaviour
         hitPower = 1;
         CPS = 1;
         x = 0f;
+        
 
         //
-        Building1Cost = 25;
+        shop1prize = 25;
         shop2prize = 125;
+        shop3prize = 450;
         amount1 = 0;
-        CPS1 = 1;
+        CPS1 = 0;
         amount2 = 0;
-        CPS2 = 5;
+        CPS2 = 0;
+        amount3 = 0;
+        CPS3 = 0;
 
         //
         //
@@ -57,11 +70,13 @@ public class Game : MonoBehaviour
         CPS = x * Time.deltaTime;
         cScore = cScore + CPS;
 
-        shop1text.text = "Building 1: " + Building1Cost;
-        shop2text.text = "Building 2: " + shop2prize;
+        shop1text.text = "Upgrade 1:\n" + shop1prize;
+        shop2text.text = "Upgrade 2:\n" + shop2prize;
+        shop3text.text = "Upgrade 3:\n" + shop3prize;
 
-        amount1Text.text = "Lvl 1: " + amount1 + "x, points/s:  " + CPS1;
-        amount2Text.text = "Lvl 2: " + amount2 + "x, points/s:  " + CPS2;
+        amount1Text.text = "Lvl:" + amount1 + "\npoints/s:  " + CPS1;
+        amount2Text.text = "Lvl:" + amount2 + "\npoints/s:  " + CPS2;
+        amount3Text.text = "Lvl:" + amount3 + "\npoints/s:  " + CPS3;
 
         upgradeText.text = "Upgrade click " + upgradePrize;
     }
@@ -72,13 +87,13 @@ public class Game : MonoBehaviour
 
     public void Shop1()
     {
-        if (cScore>=Building1Cost)
+        if (cScore>=shop1prize)
         {
-            cScore -= Building1Cost;
+            cScore -= shop1prize;
             amount1 += 1;
             CPS1 += 1;
             x += 1;
-            Building1Cost += 25;
+            shop1prize += 25;
         }
     }
 
@@ -91,6 +106,18 @@ public class Game : MonoBehaviour
             CPS2 += 5;
             x += 5;
             shop2prize += 125;
+        }
+    }
+
+    public void Shop3()
+    {
+        if (cScore >= shop3prize)
+        {
+            cScore -= shop3prize;
+            amount3 += 1;
+            CPS3 += 15;
+            x += 15;
+            shop3prize += 450;
         }
     }
 
